1d9957a2f964f667c085f73fc48237e7628e18b7
Ahmad Yunus <103646005+AhmadYunus299@users.noreply.github.com>
2024-04-01 13:09:57 +0700

    Feat : add migration resindetial block table

---
+++ database/migrations/2024_04_01_060209_create_resindential_block_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('resindential_block', function (Blueprint $table) {
++           $table->string('code_block', 20)->primary();
+            $table->unsignedInteger('id_resindential');
+            $table->string('name_block',20);
+            $table->timestamps();
+        
+            $table->foreign('id_resindential')->references('id')->on('resindential');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('resindential_block');
+    }
+};
